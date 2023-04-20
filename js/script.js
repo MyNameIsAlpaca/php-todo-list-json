840aefb9948e53af6eba1a83fd96f1962ac97da6
MyNameIsAlpaca <Saragosagabriele@gmail.com>
2023-04-20 16:27:21 +0200

    feat: base ex without bonus

--- js/script.js
+++ js/script.js
@@ -5,6 +5,42 @@ createApp({
   data(){
     return{
 
+      todoList: [],
+
+      newTodoInfo: '',
+
     }
-  }
+  },
+
+  methods: {
+    getList() {
+
+      axios.get('./server.php').then(response => {
+        
+        this.todoList = response.data;
+
+      });
+    },
+
+    newTodo(){
+      
+      let data = {
+        newTodo: this.newTodoInfo
+      };
+      
+      axios.post('./server.php', data, {headers: {'Content-Type': 'multipart/form-data'}}).then(response => {
+
+        this.getList();
+        
+      });
+
+      this.newTodoInfo = '';
+    },
+  },
+
+  mounted() {
+
+    this.getList();
+
+  },
 }).mount('#app');
--- server.php
+++ server.php
@@ -1,1 +1,30 @@
 <?php
+
+if(isset($_POST['newTodo'])) {
+
+  $newElement = [
+    "name" => $_POST['newTodo'],
+    "done" => false
+  ];
+
+  $listJson = file_get_contents('list.json');
+
+  $list = json_decode($listJson);
+
+  $list[] = $newElement;
+
+  $newListJson = json_encode($list);
+
+  file_put_contents('list.json', $newListJson);
+
+} else{
+
+  $listaJsonOriginal = file_get_contents('list.json');
+  
+  $listaOriginal = json_decode($listaJsonOriginal);
+  
+  header('Content-type: application/json');
+  
+  echo json_encode($listaOriginal);
+  
+}
